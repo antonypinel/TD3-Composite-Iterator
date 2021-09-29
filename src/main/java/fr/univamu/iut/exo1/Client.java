b1f079d69cde8cad399d2e2727e7716316608064
Antony Pinel <antony.pinel@etu.univ-amu.fr>
2021-09-29 10:04:28 +0200

    3 classes are done main is working now !

--- src/main/java/fr/univamu/iut/exo1/Client.java
+++ src/main/java/fr/univamu/iut/exo1/Client.java
@@ -2,6 +2,10 @@ package fr.univamu.iut.exo1;
 
 public class Client {
     public static void main(String[] args) {
-
+        Nombre nextDroite = new Nombre (5);
+        Nombre basGauche = new Nombre(2);
+        Nombre basDroite = new Nombre(3);
+        Expression head = new NoeudInterne('*', new NoeudInterne('-', basGauche, basDroite), nextDroite);
+        System.out.println(head.CalculateValue());
     }
 }
